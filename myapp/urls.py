42c6b5d473ab75ae6aecc4f5c54e7a649bc63170
Chhagan Kumawat <chhagankumarkumawat1212@gmail.com>
2025-03-23 16:41:19 +0530

    To change Book Discovery Call url

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('/', views.BoolDiscovery, name='booldiscovery'),
-    path('/', views.BookDiscoveryForm, name='get-in-touch-form')
+    path('/', views.BookDiscoveryForm, name='getintouchform')
 ]
